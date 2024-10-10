240c88ce8e3c65dc71da2b2873e3cede66a539c1
Marco Talento <marcotalento90@gmail.com>
2024-10-10 17:01:51 +0100

    Update database-init.sh

--- scripts/database-init.sh
+++ scripts/database-init.sh
@@ -4,10 +4,10 @@ dotnet build ./CaravelTemplate.sln
 
 # Create Database Schema Changes
 #echo "Create Database Schema"
-dotnet ef migrations add CreateApplicationSchema --startup-project src/CaravelTemplate.Migrator --output-dir Shared/Data/Migrations --project src/CaravelTemplate.Api --context ApplicationDbContext --no-build
-dotnet ef migrations add CreateIdentitySchema --startup-project src/CaravelTemplate.Migrator --output-dir Shared/Identity/Migrations --project src/CaravelTemplate.Api --context IdentityDbContext --no-build
+dotnet ef migrations add CreateApplicationSchema --startup-project src/CaravelTemplate.Migrator --output-dir Shared/Data/Migrations --project src/CaravelTemplate.Api --context ApplicationDbContext
+dotnet ef migrations add CreateIdentitySchema --startup-project src/CaravelTemplate.Migrator --output-dir Shared/Identity/Migrations --project src/CaravelTemplate.Api --context IdentityDbContext
 
 # Apply Database Schema Changes
 echo "Apply Database Changes"
-dotnet ef database update --startup-project src/CaravelTemplate.Migrator --project src/CaravelTemplate.Api --context ApplicationDbContext --no-build
-dotnet ef database update --startup-project src/CaravelTemplate.Migrator --project src/CaravelTemplate.Api --context IdentityDbContext --no-build
+dotnet ef database update --startup-project src/CaravelTemplate.Migrator --project src/CaravelTemplate.Api --context ApplicationDbContext
+dotnet ef database update --startup-project src/CaravelTemplate.Migrator --project src/CaravelTemplate.Api --context IdentityDbContext
